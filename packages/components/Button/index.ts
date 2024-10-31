0c249ad5e74dca0b8c01c82eaaba828699b29691
unknown <zhiyug@corp.longshine.com>
2024-10-31 20:36:47 +0800

    :tada: first commit

---
+++ packages/components/Button/index.ts
@@ -0,0 +1,5 @@
+import Button from './Button.vue';
+
+import {withInstall} from '@toy-element/utils/index.ts'
+
+export const ErButton = withInstall(Button)
---
+++ packages/play/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import ToyElement from 'toy-element'
+
+const app = createApp(App)
+app.use(ToyElement)
+app.mount('#app')
